a3dbb2fc6d87ecea5293ad29bb4bc1335a9280f2
Lexan <namaya438@gmail.com>
2025-05-09 08:18:58 -0400

    Initial Commit

---
+++ potato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  potato
+//
+//  Created by Noe Amaya on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Lexan Amaya, Potato")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ potato/potatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  potatoApp.swift
+//  potato
+//
+//  Created by Noe Amaya on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct potatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
